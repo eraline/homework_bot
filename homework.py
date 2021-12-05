2c4041f4712868ecbb27c4b21ff52638248f39ce
Konstantin Kintushev <k.kintushev@gmail.com>
2021-12-05 20:07:58 +0300

    Sprint 07 Project is done

--- homework.py
+++ homework.py
@@ -1,82 +1,117 @@
-...
+import logging
+import sys
+import os
+import requests
+import time
+import telegram
+from pprint import pprint
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('YANDEX_TOKEN')
+TELEGRAM_TOKEN = os.getenv('BOT_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    stream=sys.stdout,
+    filemode='a',
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+)
+
 
 def send_message(bot, message):
-    ...
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logging.info(f'Message sent successfully: {message}')
+    except Exception as error:
+        logging.error(f'Error while sending message: {error}')
 
 
 def get_api_answer(current_timestamp):
+
+    logging.debug('Getting response')
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    if response.status_code == 200:
+        response = response.json()
+        return response
+    else:
+        raise BaseException('StatusCode is not 200')
 
 
 def check_response(response):
-
-    ...
+    logging.debug('Checking response')
+    if not isinstance(response, dict):
+        raise TypeError
+    if not isinstance(response['homeworks'], list):
+        raise TypeError
+    if 'homeworks' not in response:
+        raise KeyError
+    return response['homeworks']
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    logging.debug('Start parsing hw status')
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    if not PRACTICUM_TOKEN:
+        logging.critical('Отсутствует переменная окружения PRACTICUM_TOKEN')
+        return False
+    if not TELEGRAM_TOKEN:
+        logging.critical('Отсутствует переменная окружения TELEGRAM_TOKEN')
+        return False
+    if not TELEGRAM_CHAT_ID:
+        logging.critical('Отсутствует переменная окружения TELEGRAM_CHAT_ID')
+        return False
+    return True
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
+    last_err_msg = ''
+    logging.info('Starting bot')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-
-    ...
-
+    logging.debug('Checking tokens')
+    if check_tokens() is True:
+        logging.debug('Tokens are valid')
+    else:
+        logging.critical('TOKENS ARE NOT VALID')
+        raise BaseException('Invalid tokens')
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            homeworks = check_response(response)
+            pprint(homeworks)
+            for homework in homeworks:
+                message = parse_status(homework)
+                send_message(bot, message)
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
 
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
+            logging.error(error)
             time.sleep(RETRY_TIME)
-        else:
-            ...
+            if last_err_msg != error:
+                send_message(bot, error)
 
 
 if __name__ == '__main__':
